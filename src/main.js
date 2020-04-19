5de0979bcb5170efba43ce054c2225101c8b0009
维 王 <wangwei@aiidc.com.cn>
2020-04-19 18:49:47 +0800

    修正为高德地图

--- src/main.js
+++ src/main.js
@@ -7,18 +7,35 @@ import store from './store'
 import $http from './services/queryService'
 import $nav from './utils/nav'
 import GlobalLoading from './utils/globalLoading'
-import BaiduMap from 'vue-baidu-map'
+import AMap from 'vue-amap'
 
 import './styles/'
+Vue.use(AMap)
+
+// 初始化vue-amap
+AMap.initAMapApiLoader({
+  // 高德key
+  key: 'f5212b377b6543983c0aa63d3c3af1fd',
+  // 插件集合 （插件按需引入）
+  plugin: [
+    'AMap.Autocomplete', // 输入提示插件
+    'AMap.PlaceSearch', // POI搜索插件
+    'AMap.Scale', // 右下角缩略图插件 比例尺
+    'AMap.OverView', // 地图鹰眼插件
+    'AMap.ToolBar', // 地图工具条
+    'AMap.MapType', // 类别切换控件，实现默认图层与卫星图、实施交通图层之间切换的控制
+    'AMap.PolyEditor', // 编辑 折线多，边形
+    'AMap.CircleEditor', // 圆形编辑器插件
+    'AMap.Geolocation' // 定位控件，用来获取和展示用户主机所在的经纬度位置
+  ],
+  uiVersion: '1.0.11'
+})
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
 Vue.use($http)
 Vue.use($nav)
-Vue.use(BaiduMap, {
-  ak: 'PvuZYQ8BGMIEgq9fsVrM246Qn9gSFcDg'
-})
 Vue.prototype.$globalLoading = GlobalLoading
 
 console.log('test console')
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,197 +1,233 @@
 <template>
   <div class="home-page">
-    <div class="mapbox">
-      <div class="top-search">
-        <el-input size="small" v-model="keyword" />
-      </div>
-      <baidu-map
-        :center="center"
-        :zoom="zoom"
-        :map-click="false"
-        @mousemove="syncPolygon"
-        @ready="handler"
-        style="height:800px"
-        @click="paintPolygon"
-        @rightclick="newPolygon"
-      >
-        <!-- 已经绘制的区域 -->
-        <bm-polygon
-          v-for="(path, index) of paths"
-          :path="path"
-          :key="index"
-          :stroke-color="drawConfig.strokeColor"
-          :fill-color="drawConfig.fillColor"
-          :fill-opacity="drawConfig.fillOpacity"
-          :stroke-opacity="drawConfig.strokeOpacity"
-          :stroke-weight="drawConfig.strokeWidth"
-          :editing="polygonPath.editing"
-          @lineupdate="lineupdateHandler"
-        />
-        <!-- 绘制区域 -->
-        <bm-polygon
-          v-for="(path, index) of polygonPath.paths"
-          :path="path"
-          :key="index"
-          :stroke-color="drawConfig.strokeColor"
-          :fill-color="drawConfig.fillColor"
-          :fill-opacity="drawConfig.fillOpacity"
-          :stroke-opacity="drawConfig.strokeOpacity"
-          :stroke-weight="drawConfig.strokeWidth"
-        />
-        <bm-control>
-          <el-button @click="toggle('polygonPath')" size="small">{{ polygonPath.editing ? '停止绘制' : '开始绘制' }}</el-button>
-          <el-button @click="save" size="small">保 存</el-button>
-        </bm-control>
-        <bm-local-search :keyword="keyword" :auto-viewport="true"></bm-local-search>
-      </baidu-map>
+    <div class="actions">
+      <el-button size="small" @click="isAdding = !isAdding">{{ isAdding ? '取消' : '绘制' }}</el-button>
+      <el-button size="small" @click="isEditing = !isEditing">{{ isEditing ? '取消' : '编辑' }}</el-button>
+      <el-button size="small" @click="clear">重新绘制</el-button>
+      <el-button size="small" @click="save">保存</el-button>
     </div>
-    <el-dialog
-      :visible="saveVisible"
-      title="保存"
-    >
-      <el-form>
-        <el-form-item label="名称" prop="name">
-          <el-input size="small" v-model="name" />
-        </el-form-item>
-      </el-form>
-    </el-dialog>
+    <div class="map-containe">
+      <el-amap-search-box
+        class="search-box"
+        :search-option="searchOption"
+        :on-search-result="onSearchResult"
+      ></el-amap-search-box>
+      <el-amap
+        ref="map"
+        vid="amapDemo"
+        :amap-manager="aMapManager"
+        :center="mapCenter"
+        :zoom="zoom"
+        :plugin="plugin"
+        :events="events"
+        class="amap-demo"
+      >
+        <el-amap-marker
+          v-for="(marker, index) in markers"
+          :key="index"
+          :position="marker"
+        />
+        <el-amap-polygon
+          v-for="(polygon, index) in polygons"
+          :key="index"
+          :vid="index"
+          :editable="isEditing"
+          :ref="`polygon_${index}`"
+          :path="polygon.path"
+          :draggable="polygon.draggable"
+          :events="polygon.events"
+          :strokeColor="drawConfig.strokeColor"
+          :strokeOpacity="drawConfig.strokeOpacity"
+          :strokeWeight="drawConfig.strokeWeight"
+          :fillColor="drawConfig.fillColor"
+          :fillOpacity="drawConfig.fillOpacity"
+          @end="endHandler"
+        />
+      </el-amap>
+    </div>
   </div>
 </template>
 
 <script>
-import bmPolygon from 'vue-baidu-map/components/overlays/Polygon'
-import BmLocalSearch from 'vue-baidu-map/components/search/LocalSearch.vue'
-import BmControl from 'vue-baidu-map/components/controls/Control.vue'
+import { AMapManager } from 'vue-amap'
+const amapManager = new AMapManager()
 
 export default {
-  name: 'home',
-  components: {
-    bmPolygon,
-    BmLocalSearch,
-    BmControl
-  },
   data () {
+    const _this = this
     return {
-      title: 'demo',
-      keyword: '',
-      center: { lng: 104.075707, lat: 30.516006 },
-      zoom: 14,
-      polygonPath: {
-        editing: false,
-        paths: []
+      searchOption: {
+        city: '成都',
+        citylimit: true
       },
+      mapCenter: [104.053185, 30.3755265],
+      markers: [
+        {
+          position: [104.053185, 30.3755265],
+          events: {
+            click: () => {
+              alert('click marker')
+            },
+            dragend: (e) => {
+              this.markers[0].position = [e.lnglat.lng, e.lnglat.lat]
+            }
+          },
+          visible: true,
+          draggable: false,
+          template: '<span>煎茶路街道</span>'
+        }
+      ],
+      aMapManager: amapManager,
+      zoom: 13,
+      plugin: [
+        {
+          pName: 'MapType',
+          defaultType: 0,
+          events: {
+            init (o) {
+              console.log(o)
+            }
+          }
+        },
+        {
+          pName: 'Geolocation',
+          defaultType: 0,
+          events: {
+            init (o) {
+              console.log(o)
+            }
+          }
+        }
+      ],
+      events: {
+        init: o => {
+          o.getCity(result => {
+            console.log(result)
+          })
+        },
+        moveend: () => {},
+        zoomchange: () => {},
+        click: e => {
+          if (_this.isAdding) {
+            _this.addpolygon(e)
+          }
+        }
+      },
+      polygons: [
+        {
+          draggable: true,
+          path: [],
+          events: {
+            end (e) {
+              _this.endHandler(e.target)
+            }
+          }
+        }
+      ],
       drawConfig: {
-        strokeColor: '#007ed2', // 边界颜色
-        fillColor: '#007ed2', // 区域颜色
-        fillOpacity: 0.2, // 区域透明度
-        strokeOpacity: 0.5, // 边界线透明度
-        strokeWidth: 1 // 边界线宽
+        strokeColor: '#007ed2',
+        strokeOpacity: 0.3,
+        strokeWeight: 1,
+        fillColor: '#007ed2',
+        fillOpacity: 0.2
       },
-      paths: [],
-      saveVisible: false,
-      name: ''
+      isEditing: false,
+      isAdding: false,
+      path: []
     }
   },
   mounted () {
-    this.getRange()
+    this.getData()
   },
   methods: {
-    getRange () {
+    getData () {
       this.$http.town.getRange().then(res => {
-        this.paths.push(res)
+        let points = []
+        res.map(i => {
+          if (i.lng && i.lat) {
+            points.push([i.lng, i.lat])
+          }
+        })
+        this.polygons[0].path = points
+        console.log(this.polygons)
       })
     },
-    handler ({ BMap, map }) {
-      map.enableScrollWheelZoom(true)
+    // 查询定位
+    onSearchResult (pois) {
+      let latSum = 0
+      let lngSum = 0
+      if (pois.length > 0) {
+        pois.forEach(poi => {
+          let { lng, lat } = poi
+          lngSum += lng
+          latSum += lat
+          this.markers.push([poi.lng, poi.lat])
+        })
+        let center = {
+          lng: lngSum / pois.length,
+          lat: latSum / pois.length
+        }
+        this.mapCenter = [center.lng, center.lat]
+      }
     },
-    getClickInfo (e) {
-      console.log(e.point.lng)
-      console.log(e.point.lat)
+    generatePath (points) {
+      let path = this.polygons[0].path
+      path.push(points)
+      let polygon = {
+        draggable: true,
+        path: this.path,
+        events: {
+          click: () => {
+            alert('click polygon')
+            console.log(amapManager.getComponent(0))
+            console.log(this.$refs.map.$$getCenter())
+            console.log(this.$refs.polygon_0[0].$$getPath())
+          }
+        }
+      }
+      console.log(polygon)
+      return polygon
     },
-    // 开启多边形绘制
-    toggle (name) {
-      this[name].editing = !this[name].editing
-      // 在这里做一步判断，如果有路径且开启绘制就把原来的路径清空
-      // if (this.polygonPath.paths && this.polygonPath.editing) {
-      //   this.polygonPath.paths = []
-      // }
+    // 添加节点
+    addpolygon (e) {
+      let { lng, lat } = e.lnglat
+      this.polygons.push(this.generatePath([lng, lat]))
     },
-    // 鼠标移动时
-    syncPolygon (e) {
-      if (!this.polygonPath.editing) {
-        return
-      }
-      const { paths } = this.polygonPath
-      if (!paths.length) {
-        return
-      }
-      const path = paths[paths.length - 1]
-      if (!path.length) {
-        return
-      }
-      if (path.length === 1) {
-        path.push(e.point)
-      }
-      this.$set(path, path.length - 1, e.point)
+    endHandler (polygon) {
+      let paths = polygon.getPath()
+      this.path = paths.map(i => {
+        return {
+          lng: i.lng,
+          lat: i.lat
+        }
+      })
     },
-    // 完成绘制
-    newPolygon (e) {
-      if (!this.polygonPath.editing) {
-        return
-      }
-      // 当开始绘制后把按钮调回开始绘制状态，防止绘制多个图形
-      this['polygonPath'].editing = !this['polygonPath'].editing
-      const { paths } = this.polygonPath
-      if (!paths.length) {
-        paths.push([])
-      }
-      // 回退
-      const path = paths[paths.length - 1]
-      path.pop()
-      if (path.length) {
-        paths.push([])
-      }
-      console.log(this.polygonPath.paths)
-    },
-    // 设置节点
-    paintPolygon (e) {
-      if (!this.polygonPath.editing) {
-        return
-      }
-      const { paths } = this.polygonPath
-      !paths.length && paths.push([])
-      paths[paths.length - 1].push(e.point)
-    },
-    alertpath (e) {
-      console.log(e.currentTarget.so)
-      console.log(this.polygonPath.paths[0])
+    clear () {
+      this.polygons[0].path = []
     },
     save () {
-      this.saveVisible = true
-    },
-    submit () {
       let queryData = {
         townId: 1,
-        points: this.paths[0]
+        points: this.path
       }
       this.$http.town.saveRange(queryData).then(res => {
         this.getRange()
       })
-    },
-    lineupdateHandler (e) {
-      this.polygonPath.paths[0] = e.target.getPath()
     }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-.mapbox {
-  width: 800px;
-  height: 800px;
-  margin: 0 auto
-}
-.top-search {
-  width: 300px;
+<style lang="scss">
+.map-containe {
+  position: relative;
+  width: 600px;
+  height: 600px;
+
+  .search-box {
+    position: absolute;
+    top: 25px;
+    left: 20px;
+  }
 }
 </style>
